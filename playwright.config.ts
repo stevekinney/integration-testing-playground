11f03a07d0216487b68cee0ffb968e850a0ab3ee
Steve Kinney <hello@stevekinney.net>
2023-03-22 05:22:36 -0500

    Add tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,6 +76,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
+    reuseExistingServer: true,
     port: 3000,
   },
 });
--- tests/pokemon-search.spec.ts
+++ tests/pokemon-search.spec.ts
@@ -12,3 +12,16 @@ test('visit the Pokemon page and have a search input', async ({ page }) => {
 
   expect(page.url()).toContain('/25');
 });
+
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Pokémon Search' }).click();
+  await page.getByTestId('search').click();
+  await page.getByTestId('search').type('Bulba');
+  await page.getByTestId('1').click();
+  const heading = page.getByRole('heading', { name: 'Bulbasaur' });
+  await expect(heading).toHaveScreenshot('heading.png', { maxDiffPixels: 100 });
+  await page.getByRole('cell', { name: 'Pokédex Number' }).click();
+  const label = page.getByTestId('search-label');
+  await expect(await label.textContent()).toMatchSnapshot();
+});
